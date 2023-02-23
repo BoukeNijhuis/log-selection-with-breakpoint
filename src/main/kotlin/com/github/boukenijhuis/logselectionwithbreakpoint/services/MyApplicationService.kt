6a130abf571029a8719f005739520abca6853b11
GitHub Action <action@github.com>
2023-02-23 20:12:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/boukenijhuis/logselectionwithbreakpoint/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/boukenijhuis/logselectionwithbreakpoint/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.boukenijhuis.logselectionwithbreakpoint.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.boukenijhuis.logselectionwithbreakpoint.MyBundle
 
 class MyApplicationService {
 
